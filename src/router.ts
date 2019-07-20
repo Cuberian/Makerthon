2d1a50a7ad34461f48357ba212307788f78876f9
Igor <themess@inbox.ru>
2019-07-20 18:43:51 +0500

    add LINKS

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import App from './views/App.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/Home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/',
+      name: 'App',
+      component: App
     },
     {
       path: '/About',
